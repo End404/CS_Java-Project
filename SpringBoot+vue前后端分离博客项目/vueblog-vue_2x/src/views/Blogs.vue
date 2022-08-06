f304a3884b8e4311dc7d9b4b88062e6652548805
字母表 <42495830+End404@users.noreply.github.com>
2022-08-06 16:22:12 +0800

    博客项目
    SpringBoot+vue前后端分离博客项目

---
+++ SpringBoot+vue前后端分离博客项目/vueblog-vue_2x/src/views/Blogs.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="mcontaner">
+    <Header></Header>
+
+    <div class="block">
+      <el-timeline>
+
+        <el-timeline-item timestamp="blog.created" placement="top" v-for="blog in blog">
+          <el-card>
+            <h4>
+              <router-link :to="{name: 'BlogDetail', params: {blogID: blog.id}}">
+                {{blog.title}}
+              </router-link>
+            </h4>
+            <h4>{{blog.title}}</h4>
+            <p>{{blog.description}}</p>
+          </el-card>
+        </el-timeline-item>
+
+      </el-timeline>
+
+      <el-pagination class="mpage"
+          background
+          layout="prev, pager, next"
+          :current-page="currentPage"
+          :page-size="pageSize"
+          :total="total"
+          @current-chage=page>
+      </el-pagination>
+    </div>
+
+  </div>
+
+</template>
+
+<!--  博客列表-->
+<script>
+import Header from "@/components/Header";
+
+export default {
+  name: "Blogs.vue",
+  components: {Header},
+  data() {
+    return {
+      blogs:{},
+      currentPage: 1,
+      total: 0,
+      pageSize: 5
+    }
+  },
+  methods: {
+    page(currentPage) {
+    const _this = this
+      _this.$axios.get("/blogs?currentPage=" + currentPage).then(res => {
+        console.log(res)
+        _this.blogs = res.data.data.records
+        _this.currentPage = res.data.data.current
+        _this.total = res.data.data.total
+        _this.pageSize = res.data.data.size
+
+      })
+    }
+  },
+  created() {
+    this.page(1)
+  }
+}
+</script>
+
+<style scoped>
+
+.mpage {
+  margin: 0 auto;
+  text-align: center;
+}
+
+</style>
